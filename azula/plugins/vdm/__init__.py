b2016e50117b5d2574898e5c7f849bb5ca319885
François Rozet <francois.rozet@outlook.com>
2024-08-26 22:44:42 +0200

    ✨ Add VDM plugin (#10)

---
+++ azula/plugins/vdm/__init__.py
@@ -0,0 +1,132 @@
+r"""Velocity diffusion model (VDM) plugin.
+
+This plugin depends on the `diffusion` module in the `crowsonkb/v-diffusion-pytorch
+<https://github.com/crowsonkb/v-diffusion-pytorch>`_ repository. To use it, clone the
+repository to you machine
+
+.. code-block:: console
+
+    git clone https://github.com/crowsonkb/v-diffusion-pytorch
+
+and add it to your Python path before importing the plugin.
+
+.. code-block:: python
+
+    import sys; sys.path.append("path/to/v-diffusion-pytorch")
+    ...
+    from azula.plugins import vdm
+"""
+
+__all__ = [
+    "AngularSchedule",
+    "VelocityDenoiser",
+    "list_models",
+    "load_model",
+]
+
+import math
+import torch
+import torch.nn as nn
+
+from azula.denoise import Gaussian, GaussianDenoiser
+from azula.hub import download
+from azula.nn.utils import FlattenWrapper
+from azula.noise import Schedule
+from azula.plugins.utils import RaiseMock
+from torch import Tensor
+from typing import List, Sequence, Set, Tuple
+
+try:
+    from diffusion.models import get_model  # type: ignore
+except ImportError as e:
+    get_model = RaiseMock(name="diffusion.models.get_model", error=e)
+
+# isort: split
+from . import database
+
+
+class AngularSchedule(Schedule):
+    r"""Creates an angular schedule."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.register_buffer("pi", torch.as_tensor(math.pi))
+
+    def alpha(self, t: Tensor) -> Tensor:
+        return torch.cos(self.pi / 2 * t)
+
+    def sigma(self, t: Tensor) -> Tensor:
+        return torch.sin(self.pi / 2 * t)
+
+    def forward(self, t: Tensor) -> Tuple[Tensor, Tensor]:
+        return self.alpha(t).unsqueeze(-1), self.sigma(t).unsqueeze(-1)
+
+
+class VelocityDenoiser(GaussianDenoiser):
+    r"""Creates a velocity denoiser.
+
+    Arguments:
+        backbone: A time conditional network.
+    """
+
+    def __init__(self, backbone: nn.Module):
+        super().__init__()
+
+        self.backbone = backbone
+        self.schedule = AngularSchedule()
+
+    def forward(self, x_t: Tensor, t: Tensor, **kwargs) -> Gaussian:
+        alpha_t, sigma_t = self.schedule(t)
+
+        mean = alpha_t * x_t - sigma_t * self.backbone(x_t, t, **kwargs)
+        var = sigma_t**2 / (alpha_t**2 + sigma_t**2)
+
+        return Gaussian(mean=mean, var=var)
+
+
+def list_models() -> List[str]:
+    r"""Returns the list of available pre-trained models."""
+
+    return database.keys()
+
+
+def load_model(key: str, **kwargs) -> GaussianDenoiser:
+    r"""Loads a pre-trained ADM model.
+
+    Arguments:
+        key: The pre-trained model key.
+        kwargs: Keyword arguments passed to :func:`torch.load`.
+
+    Returns:
+        A pre-trained denoiser.
+    """
+
+    kwargs.setdefault("map_location", "cpu")
+    kwargs.setdefault("weights_only", True)
+
+    url, hash_prefix, config = database.get(key)
+    state = torch.load(download(url, hash_prefix=hash_prefix), **kwargs)
+
+    denoiser = make_model(**config)
+    denoiser.backbone.wrappee.load_state_dict(state)
+    denoiser.eval()
+
+    return denoiser
+
+
+def make_model(
+    # Data
+    image_channels: int = 3,
+    image_size: int = 128,
+    # Backbone
+    key: str = "imagenet_128",
+) -> GaussianDenoiser:
+    r"""Instantiates a VDM denoiser."""
+
+    backbone = FlattenWrapper(
+        wrappee=get_model(key)(),
+        shape=(image_channels, image_size, image_size),
+    )
+
+    return VelocityDenoiser(backbone)
---
+++ azula/plugins/vdm/database.py
@@ -0,0 +1,47 @@
+r"""Pre-trained models database."""
+
+from typing import Dict, List, Tuple
+
+
+def get(key: str) -> Tuple[str, str, Dict]:
+    r"""Returns the URL, hash and config of a pre-trained model.
+
+    Arguments:
+        key: The pre-trained model key.
+    """
+
+    return URLS[key], HASHES[key], CONFIGS[key]
+
+
+def keys() -> List[str]:
+    r"""Returns the list of available pre-trained models."""
+
+    return list(URLS.keys())
+
+
+URLS = {
+    "danbooru_128x128": "https://the-eye.eu/public/AI/models/v-diffusion/danbooru_128.pth",
+    "imagenet_128x128": "https://the-eye.eu/public/AI/models/v-diffusion/imagenet_128.pth",
+    "wikiart_128x128": "https://the-eye.eu/public/AI/models/v-diffusion/wikiart_128.pth",
+    "wikiart_256x256": "https://the-eye.eu/public/AI/models/v-diffusion/wikiart_256.pth",
+    "yfcc_512x512": "https://the-eye.eu/public/AI/models/v-diffusion/yfcc_1.pth",
+    "yfcc_512x512_large": "https://the-eye.eu/public/AI/models/v-diffusion/yfcc_2.pth",
+}
+
+HASHES = {
+    "danbooru_128x128": "sha256:1728940d3531504246dbdc75748205fd8a24238a17e90feb82a64d7c8078c449",
+    "imagenet_128x128": "sha256:cac117cd0ed80390b2ae7f3d48bf226fd8ee0799d3262c13439517da7c214a67",
+    "wikiart_128x128": "sha256:b3ca8d0cf8bd47dcbf92863d0ab6e90e5be3999ab176b294c093431abdce19c1",
+    "wikiart_256x256": "sha256:da45c38aa31cd0d2680d29a3aaf2f50537a4146d80bba2ca3e7a18d227d9b627",
+    "yfcc_512x512": "sha256:a1c0f6baaf89cb4c461f691c2505e451ff1f9524744ce15332b7987cc6e3f0c8",
+    "yfcc_512x512_large": "sha256:69ad4e534feaaebfd4ccefbf03853d5834231ae1b5402b9d2c3e2b331de27907",
+}
+
+CONFIGS = {
+    "danbooru_128x128": {"key": "danbooru_128", "image_size": 128},
+    "imagenet_128x128": {"key": "imagenet_128", "image_size": 128},
+    "wikiart_128x128": {"key": "wikiart_128", "image_size": 128},
+    "wikiart_256x256": {"key": "wikiart_256", "image_size": 256},
+    "yfcc_512x512": {"key": "yfcc_1", "image_size": 512},
+    "yfcc_512x512_large": {"key": "yfcc_2", "image_size": 512},
+}
