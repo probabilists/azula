73597d9ede8549d5018f4bd95c6dd9e31fa9513e
François Rozet <francois.rozet@outlook.com>
2024-10-07 17:35:49 -0400

    🩹 Fix VDM noise schedule

--- azula/plugins/vdm/__init__.py
+++ azula/plugins/vdm/__init__.py
@@ -18,7 +18,7 @@ and add it to your Python path before importing the plugin.
 """
 
 __all__ = [
-    "AngularSchedule",
+    "CrowsonSchedule",
     "VelocityDenoiser",
     "list_models",
     "load_model",
@@ -37,30 +37,33 @@ from torch import Tensor
 from typing import List, Tuple
 
 try:
-    from diffusion.models import get_model  # type: ignore
+    import diffusion as crowson  # type: ignore
 except ImportError as e:
-    get_model = RaiseMock(name="diffusion.models.get_model", error=e)
+    crowson = RaiseMock(name="diffusion", error=e)
 
 # isort: split
 from . import database
 
 
-class AngularSchedule(Schedule):
-    r"""Creates an angular schedule."""
+class CrowsonSchedule(Schedule):
+    r"""Creates an angular noise schedule."""
 
-    def __init__(self):
+    def __init__(self, spliced: bool = False):
         super().__init__()
 
-        self.register_buffer("pi", torch.as_tensor(math.pi))
+        self.spliced = spliced
 
-    def alpha(self, t: Tensor) -> Tensor:
-        return torch.cos(self.pi / 2 * t)
-
-    def sigma(self, t: Tensor) -> Tensor:
-        return torch.sin(self.pi / 2 * t)
+    def backbone_time(self, t: Tensor) -> Tensor:
+        if self.spliced:
+            return crowson.utils.get_spliced_ddpm_cosine_schedule(t)
+        else:
+            return crowson.utils.get_ddpm_schedule(t)
 
     def forward(self, t: Tensor) -> Tuple[Tensor, Tensor]:
-        return self.alpha(t).unsqueeze(-1), self.sigma(t).unsqueeze(-1)
+        t = self.backbone_time(t)
+        alpha_t, sigma_t = crowson.utils.t_to_alpha_sigma(t)
+
+        return alpha_t.unsqueeze(-1), sigma_t.unsqueeze(-1)
 
 
 class VelocityDenoiser(GaussianDenoiser):
@@ -71,7 +74,7 @@ class VelocityDenoiser(GaussianDenoiser):
         schedule: A noise schedule.
     """
 
-    def __init__(self, backbone: nn.Module, schedule: Schedule):
+    def __init__(self, backbone: nn.Module, schedule: CrowsonSchedule):
         super().__init__()
 
         self.backbone = backbone
@@ -79,6 +82,7 @@ class VelocityDenoiser(GaussianDenoiser):
 
     def forward(self, x_t: Tensor, t: Tensor, **kwargs) -> Gaussian:
         alpha_t, sigma_t = self.schedule(t)
+        t = self.schedule.backbone_time(t)
 
         mean = alpha_t * x_t - sigma_t * self.backbone(x_t, t, **kwargs)
         var = sigma_t**2 / (alpha_t**2 + sigma_t**2)
@@ -125,11 +129,12 @@ def make_model(
 ) -> GaussianDenoiser:
     r"""Initializes a VDM denoiser."""
 
+    model = crowson.models.get_model(key)()
     backbone = FlattenWrapper(
-        wrappee=get_model(key)(),
+        wrappee=model,
         shape=(image_channels, image_size, image_size),
     )
 
-    schedule = AngularSchedule()
+    schedule = CrowsonSchedule(spliced=model.min_t == 0)
 
     return VelocityDenoiser(backbone, schedule)
