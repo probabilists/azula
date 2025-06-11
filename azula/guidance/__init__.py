46f714de412c1856363b16a207f9956e89667158
François Rozet <francois.rozet@outlook.com>
2025-06-11 13:06:45 +0200

    ✨ Add RePaint guidance

--- azula/guidance/__init__.py
+++ azula/guidance/__init__.py
@@ -3,5 +3,6 @@ r"""Guidance and posterior sampling."""
 from .diffpir import DiffPIRDenoiser  # noqa: F401
 from .dps import DPSSampler  # noqa: F401
 from .mmps import MMPSDenoiser  # noqa: F401
+from .repaint import RePaintSampler  # noqa: F401
 from .tds import TDSSampler  # noqa: F401
 from .tmpd import TMPDenoiser  # noqa: F401
---
+++ azula/guidance/repaint.py
@@ -0,0 +1,63 @@
+r"""RePaint internals.
+
+References:
+    | RePaint: Inpainting using Denoising Diffusion Probabilistic Models (Lugmayr et al., 2022)
+    | https://arxiv.org/abs/2201.09865
+"""
+
+__all__ = [
+    "RePaintSampler",
+]
+
+import torch
+
+from torch import BoolTensor, Tensor
+
+from ..denoise import GaussianDenoiser
+from ..sample import DDIMSampler
+
+
+class RePaintSampler(DDIMSampler):
+    r"""Creates a RePaint sampler.
+
+    Arguments:
+        denoiser: A Gaussian denoiser.
+        y: An observation :math:`y = m \odot x`.
+        mask: The observation mask :math:`m`.
+        iterations: The number of RePaint iterations per step.
+        kwargs: Keyword arguments passed to :class:`azula.sample.DDIMSampler`.
+    """
+
+    def __init__(
+        self,
+        denoiser: GaussianDenoiser,
+        y: Tensor,
+        mask: BoolTensor,
+        iterations: int = 3,
+        **kwargs,
+    ):
+        super().__init__(denoiser, **kwargs)
+
+        self.register_buffer("y", torch.as_tensor(y))
+        self.register_buffer("mask", torch.as_tensor(mask))
+
+        self.iterations = iterations
+
+    def step(self, x_t: Tensor, t: Tensor, s: Tensor, **kwargs) -> Tensor:
+        alpha_s, sigma_s = self.denoiser.schedule(s)
+        alpha_t, sigma_t = self.denoiser.schedule(t)
+
+        for _ in range(self.iterations):
+            x_s = super().step(x_t, t, s, **kwargs)
+            x_s = torch.where(
+                self.mask,
+                torch.normal(alpha_s * self.y, sigma_s),
+                x_s,
+            )
+
+            x_t = alpha_t * torch.normal(
+                x_s / alpha_s,
+                torch.sqrt((sigma_t / alpha_t) ** 2 - (sigma_s / alpha_s) ** 2),
+            )
+
+        return x_s
