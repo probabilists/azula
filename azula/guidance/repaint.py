🐛 Fix null gradients with torch.normal

azula/guidance/repaint.py
@@ -51,13 +51,12 @@ class RePaintSampler(DDIMSampler):
             x_s = super().step(x_t, t, s, **kwargs)
             x_s = torch.where(
                 self.mask,
-                torch.normal(alpha_s * self.y, sigma_s),
+                alpha_s * self.y + sigma_s * torch.randn_like(self.y),
                 x_s,
             )
 
-            x_t = alpha_t * torch.normal(
-                x_s / alpha_s,
-                torch.sqrt((sigma_t / alpha_t) ** 2 - (sigma_s / alpha_s) ** 2),
-            )
+            x_t = alpha_t / alpha_s * x_s + alpha_t * torch.sqrt(
+                (sigma_t / alpha_t) ** 2 - (sigma_s / alpha_s) ** 2
+            ) * torch.randn_like(x_s)
 
         return x_s

azula/plugins/flux/__init__.py
@@ -69,7 +69,7 @@ class AutoEncoder(nn.Module):
         dtype = {"dtype": self.vae.dtype, "device": self.vae.device}
 
         q_z_x = self.vae.encode(x.to(**dtype)).latent_dist
-        z = torch.normal(q_z_x.mean, q_z_x.std)
+        z = q_z_x.mean + q_z_x.std * torch.randn_like(q_z_x.mean)
         z = (z - self.shift) * self.scale
         z = rearrange(z, "... C (H h) (W w) -> ... H W (C h w)", h=2, w=2)
 

tests/test_denoise.py
@@ -51,7 +51,7 @@ def test_GaussianPosterior(isotropic: bool, batch: Sequence[int], channels: int)
     else:
         std = torch.rand(*batch, channels) + 1e-3
 
-    x = torch.normal(mean, std)
+    x = mean + std * torch.randn_like(mean)
 
     log_q = GaussianPosterior(mean, std**2).log_prob(x)
     log_p = Normal(mean, std).log_prob(x)