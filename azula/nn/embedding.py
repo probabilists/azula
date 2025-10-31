🐛 Fix numerical stability in half precision

azula/nn/embedding.py
@@ -6,11 +6,12 @@ Note:
     encoding is a static function.
 """
 
-import numpy as np
 import torch
 import torch.nn as nn
 
 from torch import Tensor
+
+from .utils import promote_dtype
 
 
 class SineEncoding(nn.Module):
@@ -34,10 +35,8 @@ class SineEncoding(nn.Module):
 
         assert features % 2 == 0
 
-        freqs = np.linspace(0, 1, features // 2)
-        freqs = omega ** (-freqs)
-
-        self.register_buffer("freqs", torch.as_tensor(freqs, dtype=torch.float32))
+        self.features = features
+        self.omega = omega
 
     def forward(self, x: Tensor) -> Tensor:
         r"""
@@ -48,12 +47,20 @@ class SineEncoding(nn.Module):
             The embedding vector :math:`e`, with shape :math:`(*, D)`.
         """
 
-        x = x.unsqueeze(dim=-1)
+        return sine_encoding(x, features=self.features, omega=self.omega)
 
-        return torch.cat(
-            (
-                torch.sin(x * self.freqs),
-                torch.cos(x * self.freqs),
-            ),
-            dim=-1,
-        )
+
+@promote_dtype
+def sine_encoding(x: Tensor, /, features: int, omega: float = 1e4) -> Tensor:
+    x = x.unsqueeze(dim=-1)
+
+    freqs = torch.linspace(0, 1, features // 2, dtype=x.dtype, device=x.device)
+    freqs = omega ** (-freqs)
+
+    return torch.cat(
+        (
+            torch.sin(x * freqs),
+            torch.cos(x * freqs),
+        ),
+        dim=-1,
+    )

azula/nn/layers.py
@@ -16,6 +16,8 @@ from einops.layers.torch import Rearrange
 from torch import Tensor
 from typing import Sequence, Union
 
+from .utils import promote_dtype
+
 
 def ConvNd(
     in_channels: int,
@@ -108,9 +110,13 @@ class LayerNorm(nn.Module):
             The standardized tensor :math:`y`, with shape :math:`(*)`.
         """
 
-        variance, mean = torch.var_mean(x, dim=self.dim, keepdim=True)
+        return layer_norm(x, dim=self.dim, eps=self.eps)
 
-        return (x - mean) * torch.rsqrt(variance + self.eps)
+
+@promote_dtype
+def layer_norm(x: Tensor, /, dim: Sequence[int], eps: float = 1e-5) -> Tensor:
+    v, m = torch.var_mean(x, dim=dim, keepdim=True)
+    return (x - m) * torch.rsqrt(v + eps)
 
 
 class RMSNorm(nn.Module):
@@ -145,9 +151,12 @@ class RMSNorm(nn.Module):
             The normalized tensor :math:`y`, with shape :math:`(*)`.
         """
 
-        irms = torch.rsqrt(torch.mean(torch.square(x), dim=self.dim, keepdim=True) + self.eps)
+        return rms_norm(x, dim=self.dim, eps=self.eps)
 
-        return x * irms
+
+@promote_dtype
+def rms_norm(x: Tensor, /, dim: Sequence[int], eps: float = 1e-5) -> Tensor:
+    return x * torch.rsqrt(torch.mean(torch.square(x), dim=dim, keepdim=True) + eps)
 
 
 def Patchify(patch_size: Sequence[int], channel_last: bool = False) -> Rearrange: