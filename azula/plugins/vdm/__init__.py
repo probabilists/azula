c3107c05361e49114d0019cd87334e65a832db3c
François Rozet <francois.rozet@outlook.com>
2026-02-02 08:59:03 +0000

    ✨ Copy code from crowsonkb/v-diffusion-pytorch

--- azula/plugins/vdm/__init__.py
+++ azula/plugins/vdm/__init__.py
@@ -1,20 +1,11 @@
 r"""Velocity diffusion model (VDM) plugin.
 
-This plugin depends on the `diffusion` module in the `crowsonkb/v-diffusion-pytorch
-<https://github.com/crowsonkb/v-diffusion-pytorch>`_ repository. To use it, clone the
-repository to your machine
-
-.. code-block:: console
-
-    git clone https://github.com/crowsonkb/v-diffusion-pytorch
-
-and add it to your Python path before importing the plugin.
-
 .. code-block:: python
 
-    import sys; sys.path.append("path/to/v-diffusion-pytorch")
-    ...
     from azula.plugins import vdm
+
+References:
+    | https://github.com/crowsonkb/v-diffusion-pytorch
 """
 
 __all__ = [
@@ -22,24 +13,20 @@ __all__ = [
     "load_model",
 ]
 
+import math
 import torch
 import torch.nn as nn
 
 from torch import Tensor
 from typing import Optional
 
-from azula.debug import RaiseMock
 from azula.denoise import Denoiser, DiracPosterior
 from azula.hub import download
 from azula.nn.utils import get_module_dtype, skip_init
 from azula.noise import Schedule, VPSchedule
 
+from ._src import get_model
 from ..utils import load_cards
-
-try:
-    import diffusion as crowson  # type: ignore
-except ImportError as e:
-    crowson = RaiseMock(name="diffusion", error=e)
 
 
 class VelocityDenoiser(Denoiser):
@@ -74,7 +61,7 @@ class VelocityDenoiser(Denoiser):
         c_in = torch.rsqrt(alpha_t**2 + sigma_t**2)
         c_out = -sigma_t * torch.rsqrt(alpha_t**2 + sigma_t**2)
         c_skip = alpha_t * torch.rsqrt(alpha_t**2 + sigma_t**2)
-        c_time = crowson.utils.alpha_sigma_to_t(alpha_t, sigma_t).flatten()
+        c_time = torch.atan2(sigma_t, alpha_t).flatten() / math.pi * 2
 
         dtype = get_module_dtype(self.backbone)
 
@@ -119,6 +106,6 @@ def make_model(
 ) -> Denoiser:
     r"""Initializes a VDM denoiser."""
 
-    backbone = crowson.models.get_model(model)()
+    backbone = get_model(model)()
 
     return VelocityDenoiser(backbone)
---
+++ azula/plugins/vdm/_src/__init__.py
@@ -0,0 +1,4 @@
+# Adapted from
+# https://github.com/crowsonkb/v-diffusion-pytorch/tree/ffabbb1a897541fa2a3d034f397c224489d97b39/diffusion/models
+
+from .models import get_model, get_models  # noqa: F401
