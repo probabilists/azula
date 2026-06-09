eca441d2beccd1e6094d704542b9a9ed8904d10e
OmerRochman <o.rochman@uliege.be>
2026-06-09 14:33:18 -0400

    PPR sampler

--- azula/guidance/__init__.py
+++ azula/guidance/__init__.py
@@ -6,6 +6,7 @@ from .dps import DPSSampler  # noqa: F401
 from .jfps import JFPSDenoiser  # noqa: F401
 from .mmps import MMPSDenoiser  # noqa: F401
 from .pgdm import PGDMSampler  # noqa: F401
+from .ppr import PPRSampler  # noqa: F401
 from .repaint import RePaintSampler  # noqa: F401
 from .tds import TDSSampler  # noqa: F401
 from .tmpd import TMPDenoiser  # noqa: F401
